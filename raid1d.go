3e37da0fa1471106fd03f3d72b19963f0879b41e
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-19 12:54:01 +0600

    jasd

---
+++ raid1d.go
@@ -0,0 +1,33 @@
+ 
+package student
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func Raid1d(x, y int) {
+	if x < 0 || y < 0 {
+		return
+	}
+	for y1 := 1; y1 <= y; y1++ {
+		for x1 := 1; x1 <= x; x1++ {
+			var isTop = y1 == 1
+			var isBottom = y1 == y
+			var isLeft = x1 == 1
+			var isRight = x1 == x
+
+			if isTop || isBottom || isLeft || isRight {
+				if (isLeft && isTop) || (isLeft && isBottom ) {
+					z01.PrintRune('A')
+				} else if (isRight && isTop) || (isBottom && isRight) {
+					z01.PrintRune('C')
+				} else {
+					z01.PrintRune('B')
+				}
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
