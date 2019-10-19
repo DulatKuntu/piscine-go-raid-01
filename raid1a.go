cc36192bdd7a8326b34f3a7af44cf634d6e1a7de
DulatKuntu <dulaizzhambula@gmail.com>
2019-10-19 12:32:50 +0600

    asaa

--- raid1a.go
+++ raid1a.go
@@ -7,24 +7,24 @@ func Raid1a(x, y int) {
 	y1 := y
 	for y = y1; y > 0; y-- {
 		for x = x1; x > 0; x-- {
-			if x == x1 {
+			if x == 1 {
 				if y == y1 {
 					z01.PrintRune(rune('o'))
+					z01.PrintRune(rune('\n'))
 				} else if y == 1 {
 					z01.PrintRune(rune('o'))
+					z01.PrintRune(rune('\n'))
 				} else {
 					z01.PrintRune(rune('|'))
+					z01.PrintRune(rune('\n'))
 				}
-			} else if x == 1 {
+			} else if x == x1 {
 				if y == y1 {
 					z01.PrintRune(rune('o'))
-					z01.PrintRune(rune('\n'))
 				} else if y == 1 {
 					z01.PrintRune(rune('o'))
-					z01.PrintRune(rune('\n'))
 				} else {
 					z01.PrintRune(rune('|'))
-					z01.PrintRune(rune('\n'))
 				}
 			} else {
 				if y == y1 {
