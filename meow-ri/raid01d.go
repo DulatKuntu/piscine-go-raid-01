d98e8ae352a4b8a1d2726b511cf138ff754f80f6
meow-ri <umba.araika19@gmail.com>
2019-10-21 00:30:55 +0600

    optimization2

--- meow-ri/raid01d.go
+++ meow-ri/raid01d.go
@@ -10,35 +10,36 @@ func Raid1d(x, y int) {
 	var bottomRight rune = 'C'
 	var vert rune = 'B'
 	var hori rune = 'B'
-
-	for j := 1; j <= y; j++ { //j is number of line
-		for i := 1; i <= x; i++ { //i is number of column
-			if j == 1 { //if it is top line
-				if i == 1 { //if it is left column
-					z01.PrintRune(topLeft)
-				} else if i == x {
-					z01.PrintRune(topRight)
+	if x > 0 && y > 0 {
+		for j := 1; j <= y; j++ { //j is number of line
+			for i := 1; i <= x; i++ { //i is number of column
+				if j == 1 { //if it is top line
+					if i == 1 { //if it is left column
+						z01.PrintRune(topLeft)
+					} else if i == x {
+						z01.PrintRune(topRight)
+					} else {
+						z01.PrintRune(hori)
+					}
+				} else if j == y {
+					if i == 1 { //if it is left column
+						z01.PrintRune(bottomLeft)
+					} else if i == x {
+						z01.PrintRune(bottomRight)
+					} else {
+						z01.PrintRune(hori)
+					}
 				} else {
-					z01.PrintRune(hori)
-				}
-			} else if j == y {
-				if i == 1 { //if it is left column
-					z01.PrintRune(bottomLeft)
-				} else if i == x {
-					z01.PrintRune(bottomRight)
-				} else {
-					z01.PrintRune(hori)
-				}
-			} else {
-				if i == 1 { //if it is left column
-					z01.PrintRune(vert)
-				} else if i == x {
-					z01.PrintRune(vert)
-				} else {
-					z01.PrintRune(' ')
+					if i == 1 { //if it is left column
+						z01.PrintRune(vert)
+					} else if i == x {
+						z01.PrintRune(vert)
+					} else {
+						z01.PrintRune(' ')
+					}
 				}
 			}
+			z01.PrintRune('\n')	
 		}
-		z01.PrintRune('\n')
 	}
 }
