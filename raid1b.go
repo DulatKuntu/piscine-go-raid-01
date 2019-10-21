2d067bc9663720606ae5ee984e7549192552add7
meow-ri <umba.araika19@gmail.com>
2019-10-21 15:57:21 +0600

    refactoring

--- raid1b.go
+++ raid1b.go
@@ -8,24 +8,27 @@ func Raid1b(x, y int) {
 	if x < 0 || y < 0 {
 		return
 	}
+	
 	for y1 := 1; y1 <= y; y1++ {
 		for x1 := 1; x1 <= x; x1++ {
+			
 			var isTop = y1 == 1
 			var isBottom = y1 == y
 			var isLeft = x1 == 1
 			var isRight = x1 == x
-
-			if isTop || isBottom || isLeft || isRight {
-				if (isLeft && isTop) || (isRight && isBottom && y != 1 && x != 1) {
-					z01.PrintRune('/')
-				} else if (isRight && isTop) || (isBottom && isLeft) {
-					z01.PrintRune('\\')
-				} else {
-					z01.PrintRune('*')
-				}
+			
+			if (isLeft && isTop) || (isRight && isBottom && y != 1 && x != 1) {
+				z01.PrintRune('/')
+				continue
+			} else if (isRight && isTop) || (isBottom && isLeft) {
+				z01.PrintRune('\\')
+				continue
 			} else {
-				z01.PrintRune(' ')
-			}
+				z01.PrintRune('*')
+				continue
+			} 
+
+			z01.PrintRune(' ')
 		}
 		z01.PrintRune('\n')
 	}
